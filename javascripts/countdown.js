2f5fd6ba3e84abeda6b3d51b928c032d2cefbfc8
syzspectroom <syzspectroom@gmail.com>
2013-09-25 02:09:56 +0300

    refactoring countdown timer

--- javascripts/countdown.js
+++ javascripts/countdown.js
@@ -1,45 +1,41 @@
-$(document).ready(function(){
-  // set the date we're counting down to
-  var target_date = new Date("Oct 02, 2013").getTime();
+(function ($) {
 
-  // variables for time units
-  var days, hours, minutes, seconds;
+    $.fn.countdown = function( options ) {
+      var settings = $.extend({
+          date: new Date("Oct 02, 2013").getTime()
+      }, options );
 
-  // get tag element
-  var countdown = $('#countdown_container');
-  // update the tag with id "countdown" every 1 second
-  setInterval(function () {
+      var days, hours, minutes, seconds;
+      var that = $(this);
+      setInterval(function () {
+        // find the amount of "seconds" between now and target
+        var current_date = new Date().getTime();
+        var seconds_left = (settings.date - current_date) / 1000;
 
-      // find the amount of "seconds" between now and target
-      var current_date = new Date().getTime();
-      var seconds_left = (target_date - current_date) / 1000;
+        // do some time calculations
+        days = parseInt(seconds_left / 86400);
+        seconds_left = seconds_left % 86400;
 
-      // do some time calculations
-      days = parseInt(seconds_left / 86400);
-      seconds_left = seconds_left % 86400;
+        hours = parseInt(seconds_left  / 3600);
+        seconds_left = seconds_left % 3600;
 
-      hours = parseInt(seconds_left / 3600);
-      seconds_left = seconds_left % 3600;
+        minutes = parseInt(seconds_left / 60);
+        seconds = parseInt(seconds_left % 60);
 
-      minutes = parseInt(seconds_left / 60);
-      seconds = parseInt(seconds_left % 60);
-
-      var addZerro = function(time) {
-        if (time < 10) {
-          return '0';
+        var addZerro = function(time) {
+          if (time < 10) {
+            return '0'+time;
+          }
+          return time;
         }
-        return '';
-      }
+        that.html($('<div>', {class: 'days', text: addZerro(days)}))
+          .append($('<div>', {class: 'hours colons', text: addZerro(hours)}))
+          .append($('<div>', {class: 'minutes colons', text: addZerro(minutes)}))
+          .append($('<div>', {class: 'seconds colons', text: addZerro(seconds)}));
+      }, 1000);
+    };
+}( jQuery ));
 
-      zeroHours = addZerro(hours);
-      zeroMinutes = addZerro(minutes);
-      zeroSeconds = addZerro(seconds);
-
-      // format countdown string + set tag value
-      var text = '<div class="days">'+days+'</div><div class="hours colons">'+ zeroHours + hours + '</div><div class="minutes colons">'+ zeroMinutes+minutes +'</div><div class="seconds colons">'+ zeroSeconds+seconds + '</div>';
-
-      countdown.html(text)
-
-  }, 1000);
-  // $('#countdown_container').html()
-})
+$(function() {
+  $('#countdown_container').countdown();
+});
