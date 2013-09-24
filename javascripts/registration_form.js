cb024a663c2979e8a0f8a8ec24c9f5af3759ae6e
infernalmaster <infernalmaster1@gmail.com>
2013-09-24 21:38:05 +0300

    form fixed

--- javascripts/registration_form.js
+++ javascripts/registration_form.js
@@ -1,20 +1,12 @@
 $('#registration').on('submit', function(event){
     $('.btn-submit').prop('disabled', true);
     event.preventDefault();
-    var values = $(this).serialize();
-    var url    = $(this).attr('action');
+    var values = $(this).serialize(),
+        url    = $(this).attr('action'),
+        jqxhr = $.post(url, values, function(){}, 'text');
 
-    var jqxhr = $.post(url, values, function(){}, 'text');
-
-    jqxhr.done(function(data){
-      console.log(data);
-    });
-
-    jqxhr.fail(function(jqXHR, textStatus, errorThrown){
-      console.log(textStatus);
-    });
+   
     jqxhr.always(function(jqXHR, textStatus, errorThrown){
-      $('#Registration').addClass('success');
+      $('.slide3').addClass('success');
     });
-
 });
